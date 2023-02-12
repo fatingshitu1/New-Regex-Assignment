4b0ee53dec99c1feeaddebfd5630015079c9f65c
shitalfating <shitalfating84@gmail.com>
2023-02-12 23:24:35 +0530

    Refactored: Checking Valid Email

--- src/com/bridgelabz/regex/RegexAssignment.java
+++ src/com/bridgelabz/regex/RegexAssignment.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class RegexAssignment {
     static Pattern pattern = Pattern.compile("[A-Z]{3}$");
     static Pattern pattern1 = Pattern.compile("[A-Z]{3,}$");
+    static Pattern pattern2 = Pattern.compile("^[A-Za-z]{0,}+@[a-z]{5}[.]*[co][a-z]*$");
 
     public static void main(String[] args) {
         String testString = "SAI";
@@ -21,6 +22,13 @@ public class RegexAssignment {
             System.out.println("Valid last Name");
         } else {
             System.out.println("Invalid Last Name");
+        }
+        testString = "Shital@gmail.com";
+        matcher = pattern2.matcher(testString);
+        if (matcher.matches()) {
+            System.out.println("Valid email");
+        } else {
+            System.out.println("Invalid email");
         }
     }
 }
