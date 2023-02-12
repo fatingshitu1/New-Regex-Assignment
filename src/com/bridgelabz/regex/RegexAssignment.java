9336e0fe4daf7bf99e55d7881c23b7d493452331
shitalfating <shitalfating84@gmail.com>
2023-02-12 23:38:34 +0530

    Refactored: Checking Valid Mobile Number

--- src/com/bridgelabz/regex/RegexAssignment.java
+++ src/com/bridgelabz/regex/RegexAssignment.java
@@ -7,6 +7,7 @@ public class RegexAssignment {
     static Pattern pattern = Pattern.compile("[A-Z]{3}$");
     static Pattern pattern1 = Pattern.compile("[A-Z]{3,}$");
     static Pattern pattern2 = Pattern.compile("^[A-Za-z]{0,}+@[a-z]{5}[.]*[co][a-z]*$");
+    static Pattern pattern3 = Pattern.compile("^[0-9]{2}[ ][0-9]{0,}$");
 
     public static void main(String[] args) {
         String testString = "SAI";
@@ -29,6 +30,13 @@ public class RegexAssignment {
             System.out.println("Valid email");
         } else {
             System.out.println("Invalid email");
+        }
+        testString = "91 9284774178";
+        matcher = pattern3.matcher(testString);
+        if (matcher.matches()) {
+            System.out.println("Valid Mobile Number");
+        } else {
+            System.out.println("Invalid Mobile Number");
         }
     }
 }
